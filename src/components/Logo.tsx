e90cdb047901b90be990d2f9fa43e94b032a4eb3
Maarten van den Hooven <maartenv@kontent.ai>
2025-08-04 22:38:23 +0200

    preview true

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import { Link } from "react-router-dom";
 
 const Logo: FC = () => (
-  <Link to="/">
+  <Link to="/?preview=true">
     <div className="flex gap-4 items-center">
       <img 
         src="https://assets-au-01.kc-usercontent.com/ab00c5f7-85bf-02cf-55e1-a3d020897258/a6d2552f-7af1-44f3-8e63-38c38e08884f/am_logo_rgb.38aed1190173.png"
